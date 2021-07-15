69dafac0e828a4f181a11e3cf087a995e016dc24
Robin Calamatta <calamatta89@gmail.com>
2021-07-15 00:12:26 -0400

    Add files via upload

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,160 +1,143 @@
-import React, { Component, useState, useEffect } from "react";
-import { render } from "react-dom";
-import "./App.css";
-import Button from "react-bootstrap/Button";
+import React, { useState, useEffect } from "react";
+import { Select, Spin, Card, Button, List, Space, Typography } from "antd";
+import "antd/dist/antd.css";
+import axios from "axios";
 
-function SearchBar() {
-  const [make, setMake] = useState("Select Make");
-  const [type, setType] = useState("Select Type");
-  const [year, setYear] = useState("Select Year");
+const SearchBar = () => {
+  const [make, setMake] = useState("ASTON MARTIN");
+  const [type, setType] = useState(null);
+  const [year, setYear] = useState(new Date().getFullYear());
+
   const [result, setResult] = useState([]);
   const [typeResult, setTypeResult] = useState([]);
-
   const [finishingResults, setFinishingResults] = useState([]);
 
-  useEffect(() => {
-    getMakes();
+  const { Option } = Select;
 
-    fetch(
-      `https://vpic.nhtsa.dot.gov/api/vehicles/GetVehicleTypesForMake/${make}?format=json`
-    )
-      .then((response) => response.json())
-      .then((data) => setTypeResult(data.Results));
-  }, []);
-
-  /************************************* Get the Makes ************************************************** */
-  function getMakes() {
-    fetch(`https://vpic.nhtsa.dot.gov/api/vehicles/GetAllMakes?format=json`)
-      .then((response) => response.json())
-      .then((data) => setResult(data.Results));
-  }
-  /************************************************************************************************************* */
-
-  //fetch the new API
-  function fetchApi() {
-    console.log("Fetching API");
-    if (type === "Select Type" && year === "Select Year") {
-      console.log("Fetching API");
-      fetch(
-        `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/${make}?format=json`
-      )
-        .then((response) => response.json())
-        .then((data) => setFinishingResults(data.Results));
-    } else if (year === "Select Year") {
-      fetch(
-        `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeYear/make/${make}/vehicletype/${type}?format=json`
-      )
-        .then((response) => response.json())
-        .then((data) => setFinishingResults(data.Results));
-    } else if (type === "Select Type") {
-      fetch(
-        `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeYear/make/${make}/modelyear/${year}?format=json`
-      )
-        .then((response) => response.json())
-        .then((data) => setFinishingResults(data.Results));
-    } else {
-      console.log("Fetching API");
-      fetch(
-        `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeYear/make/${make}/modelyear/${year}?format=json`
-      )
-        .then((response) => response.json())
-        .then((data) => setFinishingResults(data.Results));
-    }
-  }
-
-  //read out the items
-  const makeItems = result.map((maker) => (
-    <option
-      onClick={(e) => setTypes(e.target.value)}
-      key={maker.Make_ID}
-      value={maker.Make_Name.toString()}
-    >
-      {maker.Make_Name.toString()}
-    </option>
-  ));
-
-  //read out the results
-  const allResults = finishingResults.map((noner) => (
-    <li key={noner.Model_ID}>
-      {noner.Make_Name.toString()}: {noner.Model_Name.toString()}
-    </li>
-  ));
-
-  const typeItems = typeResult.map((types) => (
-    <option
-      key={types.VehicleTypeName}
-      value={types.VehicleTypeName.toString()}
-    >
-      {types.VehicleTypeName.toString()}
-    </option>
-  ));
-
-  function setTypes(e) {
-    console.log("Setting type");
-
-    fetch(
-      `https://vpic.nhtsa.dot.gov/api/vehicles/GetVehicleTypesForMake/${make}?format=json`
-    )
-      .then((response) => response.json())
-      .then((data) => setTypeResult(data.Results));
-  }
+  const { Text, Title } = Typography;
 
   let array = [];
-  function setTheYear() {
+
+  if (array.length === 0) {
     for (let i = 1900; i <= 2021; i++) {
       array.push(i);
     }
   }
-  setTheYear();
-  const yearItems = array.map((item, i) => <option key={i}>{item}</option>);
 
-  const reset = () => {
-    setMake("Select Make");
-    setType("Select Type");
-    setYear("Select Year");
-    document.getElementById("makeID").value = "Select Make";
-    document.getElementById("typeID").value = "Select Type";
-    document.getElementById("yearID").value = "Select Year";
+  console.log(finishingResults);
 
-    document.getElementById("results").innerHTML = " ";
+  useEffect(() => {
+    axios
+      .get(`https://vpic.nhtsa.dot.gov/api/vehicles/GetAllMakes?format=json`)
+      .then((response) => setResult(response.data.Results))
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://vpic.nhtsa.dot.gov/api/vehicles/GetVehicleTypesForMake/${make}?format=json`
+      )
+      .then((response) => setTypeResult(response.data.Results))
+      .catch((err) => console.log(err));
+  }, [make]);
+
+  const finish = () => {
+    let url = `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeYear/make/${make}/modelyear/${year}/vehicletype/${type}?format=json`;
+    if (type === null) {
+      url = `https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeYear/make/${make}/modelyear/${year}?format=json`;
+    }
+    axios
+      .get(url)
+      .then((response) => setFinishingResults(response.data.Results))
+      .catch((err) => console.log(err));
   };
 
   return (
-    <div>
-      <select id="makeID" onChange={(e) => setMake(e.target.value)}>
-        <option>Select Make</option>
-        {makeItems}
-      </select>
-      <select id="typeID" onChange={(e) => setType(e.target.value)}>
-        <option>Select Type</option>
-        {typeItems}
-      </select>
-      <select id="yearID" onChange={(e) => setYear(e.target.value)}>
-        <option>Select Year</option>
-        {yearItems}
-      </select>
-      <div>
-        <Button
-          className="button"
-          variant="primary"
-          size="lg"
-          onClick={fetchApi}
+    <>
+      {result.length > 0 ? (
+        <Card
+          title="Motor Vehicle Form"
+          bordered={false}
+          style={{ width: "50%", margin: "auto" }}
         >
-          Search
-        </Button>
-        <Button
-          className="button"
-          variant="secondary"
-          size="lg"
-          onClick={reset}
-        >
-          Clear Selections
-        </Button>
-      </div>
-
-      <ul id="results">{allResults}</ul>
-    </div>
+          <Select
+            defaultValue={result[0].Make_Name}
+            style={{ width: "50%", paddingBottom: "10px", margin: "auto" }}
+            onChange={(value) => setMake(value)}
+          >
+            {result.map((maker) => {
+              return (
+                <Option key={maker.Make_ID} value={maker.Make_Name}>
+                  {maker.Make_Name}
+                </Option>
+              );
+            })}
+          </Select>
+          <br />
+          <Select
+            placeholder="Select a model type"
+            style={{ width: "50%", paddingBottom: "10px", margin: "auto" }}
+            onChange={(value) => setType(value)}
+          >
+            {typeResult.map((types) => {
+              return (
+                <Option key={types.VehicleTypeName}>
+                  {types.VehicleTypeName}
+                </Option>
+              );
+            })}
+          </Select>
+          <br />
+          <Select
+            defaultValue={year}
+            style={{ width: "50%", paddingBottom: "10px", margin: "auto" }}
+            onChange={(value) => setYear(value)}
+          >
+            {array.map((yearList) => {
+              return <Option key={yearList}>{yearList}</Option>;
+            })}
+          </Select>
+          <br />
+          <Button onClick={finish}>Search</Button>
+        </Card>
+      ) : (
+        <Spin />
+      )}
+      {finishingResults.length !== 0 && (
+        <List
+          header={<Title level={4}>Results</Title>}
+          bordered
+          dataSource={finishingResults}
+          renderItem={(item) => (
+            <List.Item>
+              <Space>
+                <Text>
+                  <b>Make ID:</b> {item.Make_ID} |&nbsp;
+                </Text>
+                <Text>
+                  <b>Make Name:</b> {item.Make_Name} |&nbsp;
+                </Text>
+                <Text>
+                  <b>Model ID:</b> {item.Model_ID} |&nbsp;
+                </Text>
+                <Text>
+                  <b>Model Name:</b> {item.Model_Name} |&nbsp;
+                </Text>
+                <Text>
+                  <b>Vehicle Type ID:</b> {item.VehicleTypeId} |&nbsp;
+                </Text>
+                <Text>
+                  {item.VehicleTypeName ? "Yes" : "no"}
+                  <b>Vehicle Type:</b> {item.VehicleTypeName}
+                </Text>
+              </Space>
+            </List.Item>
+          )}
+        />
+      )}
+    </>
   );
-}
+};
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import SearchBar from "./SearchBar";
-import Title from "./Title";
 import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
+import SearchBar from "./SearchBar";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Title />
     <SearchBar />
   </React.StrictMode>,
   document.getElementById("root")
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
